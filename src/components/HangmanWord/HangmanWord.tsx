3cc4e4c9daa5442e559d48b3848c01495d8a5eca
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-07-17 16:22:21 +0300

    ✨ Added HangmanWord Component

--- src/components/HangmanWord/HangmanWord.tsx
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -1,8 +1,24 @@
+import styles from "./HangmanWord.module.css";
 
 const HangmanWord = () => {
-  return (
-    <div>HangmanWord</div>
-  )
-}
+  const wordToGuess = "test";
+  const gussedLetters = ["t", "e"];
 
-export default HangmanWord
+  return (
+    <div className={styles.container}>
+      {wordToGuess.split("").map((letter, index) => (
+        <span key={index} style={{ borderBottom: ".1em solid black" }}>
+          <span
+            style={{
+              visibility: gussedLetters.includes(letter) ? "visible" : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
